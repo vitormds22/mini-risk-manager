2d02faac8f8f7d6bdffb941191dd04b8bf65f5a8
Vitor Machado <machado@pop-os.localdomain>
2022-07-15 15:37:13 -0300

    changing name of the columns

--- priv/repo/migrations/20220715175013_add_audit_table.exs
+++ priv/repo/migrations/20220715175013_add_audit_table.exs
@@ -4,9 +4,9 @@ defmodule MiniRiskManager.Repo.Migrations.AddAuditTable do
   def change do
     create table(:audits, primary_key: false) do
       add :id, :uuid, primary_key: true
-      add :input_risk_analysis, :map, null: true
-      add :input_transfers, :map, null: true
-      add :input_mini_risk_manager, :map, null: true
+      add :input_params, :map, null: true
+      add :model_input, :map, null: true
+      add :is_valid, :boolean
     end
   end
 end
