3c8898e7199e1a33f2975bb9a3c8f90c99d0b3ac
Vitor Machado <33588882+vitormds22@users.noreply.github.com>
2022-07-15 16:02:20 -0300

    feat: add audits table (#1)
    * creating audit table with jsonb columns
    
    * changing name of the columns
    
    * adding model_response column and set not null for all
    
    * Update priv/repo/migrations/20220715175013_add_audit_table.exs
    
    Co-authored-by: Claudio Stahl <claudioarosseto@gmail.com>
    
    Co-authored-by: Vitor Machado <machado@pop-os.localdomain>
    Co-authored-by: Claudio Stahl <claudioarosseto@gmail.com>

---
+++ priv/repo/migrations/20220715175013_add_audit_table.exs
@@ -0,0 +1,15 @@
+defmodule MiniRiskManager.Repo.Migrations.AddAuditTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:audits, primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :input_params, :map, null: false
+      add :model_input, :map, null: false
+      add :model_response, :map, null: false
+      add :is_valid, :boolean, null: false
+      
+      timestamps(updated_at: false)
+    end
+  end
+end
