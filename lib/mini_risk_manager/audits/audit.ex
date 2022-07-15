7885d8eca16429424d929406fa34a28bcaa5c861
Vitor Machado <vitor.machado@stone.com.br>
2022-07-15 17:08:04 -0300

    feat: add model audit

---
+++ lib/mini_risk_manager/audits/audit.ex
@@ -0,0 +1,22 @@
+defmodule MiniRiskManager.Audit do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, Ecto.UUID, autogenerate: true}
+  @required_keys ~w(input_params model_input model_response is_valid)a
+
+  schema "audits" do
+    field :input_params, :map
+    field :model_input, :map
+    field :model_response, :map
+    field :is_valid, :boolean
+
+    timestamps(updated_at: false)
+  end
+
+  def create_changeset(audit, attrs) do
+    audit
+    |> cast(attrs, @required_keys)
+    |> validate_required(@required_keys)
+  end
+end
