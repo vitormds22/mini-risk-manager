0b5b0c85c0ead9ec94f701bf03abda865bf99ffe
Vitor Machado <vitor.machado@stone.com.br>
2022-07-18 15:00:40 -0300

    add dialyxir lib

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule MiniRiskManager.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.6"}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false}
     ]
   end
 
