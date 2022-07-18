f420745da7dabfca4e3555ed7522c59d5d295730
Vitor Machado <vitor.machado@stone.com.br>
2022-07-18 15:44:33 -0300

    add spec on error view file

--- lib/mini_risk_manager_web/views/error_view.ex
+++ lib/mini_risk_manager_web/views/error_view.ex
@@ -1,5 +1,5 @@
 defmodule MiniRiskManagerWeb.ErrorView do
-  use MiniRiskManagerWeb, :view
+  # use MiniRiskManagerWeb, :view
 
   # If you want to customize a particular status code
   # for a certain format, you may uncomment below.
