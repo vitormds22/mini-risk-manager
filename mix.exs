c3aae61c21680e77d8451f1533eb56f89f8e244a
Vitor Machado <vitor.machado@stone.com.br>
2022-07-18 15:10:54 -0300

    add sobelow lib

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule MiniRiskManager.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.8", only: :dev}
     ]
   end
 
@@ -59,7 +60,13 @@ defmodule MiniRiskManager.MixProject do
       setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      "code.quality": [
+        "format --check-formatted",
+        "credo --strict",
+        "sobelow --config",
+        "dialyzer"
+      ]
     ]
   end
 end
