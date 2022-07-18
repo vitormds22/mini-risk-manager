8a3dc5bf088114a61bba1ef5314c51b0fed1a57c
Vitor Machado <vitor.machado@stone.com.br>
2022-07-18 15:40:30 -0300

    add type for fields in schema audit

--- lib/mini_risk_manager/cashout/models/audit.ex
+++ lib/mini_risk_manager/cashout/models/audit.ex
@@ -1,6 +1,18 @@
 defmodule MiniRiskManager.Cashout.Models.Audit do
+  @moduledoc """
+  Model for table Audits in DB.
+  Storage all calls made for the model
+  """
   use Ecto.Schema
   import Ecto.Changeset
+
+  @type t() :: %__MODULE__{
+    id: Ecto.UUID.t(),
+    input_params: map(),
+    model_input: ModelInput.t(),
+    model_response: ModelResponse.t(),
+    is_valid: String.t(),
+  }
 
   @primary_key {:id, Ecto.UUID, autogenerate: true}
   @required_keys ~w(input_params model_input model_response is_valid)a
@@ -14,6 +26,7 @@ defmodule MiniRiskManager.Cashout.Models.Audit do
     timestamps(updated_at: false)
   end
 
+  @spec create_changeset(map()) :: Ecto.Changeset.t()
   def create_changeset(attrs) do
     %__MODULE__{}
     |> cast(attrs, @required_keys)
