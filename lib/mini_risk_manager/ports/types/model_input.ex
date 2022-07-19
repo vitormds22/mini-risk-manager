b2f0344806179a7f1c9c02fe8808814bda3012eb
Vitor Machado <vitor.machado@stone.com.br>
2022-07-19 11:14:13 -0300

    feat: creat structs for input and response from model

--- lib/mini_risk_manager/ports/types/model_input.ex
+++ lib/mini_risk_manager/ports/types/model_input.ex
@@ -1,3 +1,12 @@
 defmodule MiniRiskManager.Ports.Types.ModelInput do
+  @type t() :: %__MODULE__{
+    id: Ecto.UUID.t(),
+    operation_type: Ecto.Enum, values: [:inbound_pix_payment, :inbound_external_transfer],
+    amount: integer(),
+    balance: integer(),
+    account_type: Ecto.Enum, values: [:CC, :PP, :PG] | nil,
+    sum_amount_last_24h: integer()
+  }
 
+  defstruct ~w(operation_type amount balance account_type sum_amount_last_24h)a
 end
--- lib/mini_risk_manager/ports/types/model_response.ex
+++ lib/mini_risk_manager/ports/types/model_response.ex
@@ -1,3 +1,10 @@
 defmodule MiniRiskManager.Ports.Types.ModelResponse do
 
+  @type t() :: %__MODULE__{
+    id: Ecto.UUID.t(),
+    is_valid: boolean(),
+    metadata: map()
+  }
+
+  defstruct ~w(is_valid metadata)a
 end
