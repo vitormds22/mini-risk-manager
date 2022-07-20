fb17f12a56e4f973e642556625e5a4c5ef3731be
Vitor Machado <vitor.machado@stone.com.br>
2022-07-20 14:18:17 -0300

    chore: mix format

--- lib/mini_risk_manager/ports/model_port.ex
+++ lib/mini_risk_manager/ports/model_port.ex
@@ -2,8 +2,9 @@ defmodule MiniRiskManager.Ports.ModelPort do
   @moduledoc """
   Port for call fraud model
   """
-  
-  @callback call_model(ModelInput.t()) :: {:ok, ModelResponse.t() | {:error, :bad_request | :request_failed}}
+
+  @callback call_model(ModelInput.t()) ::
+              {:ok, ModelResponse.t() | {:error, :bad_request | :request_failed}}
 
   def call_model(params) do
     adapter().call_model(params)
--- lib/mini_risk_manager/ports/types/model_response.ex
+++ lib/mini_risk_manager/ports/types/model_response.ex
@@ -1,9 +1,8 @@
 defmodule MiniRiskManager.Ports.Types.ModelResponse do
-
   @type t() :: %__MODULE__{
-    is_valid: boolean(),
-    metadata: map()
-  }
+          is_valid: boolean(),
+          metadata: map()
+        }
 
   defstruct ~w(is_valid metadata)a
 end
