3d6cb368f922c98ad764cbd3404caf346914bf9a
Vitor Machado <vitor.machado@stone.com.br>
2022-07-21 17:48:41 -0300

    feat: create functions for tranfers adapter

--- lib/mini_risk_manager/ports/balance_bloker_port.ex
+++ lib/mini_risk_manager/ports/balance_bloker_port.ex
@@ -2,4 +2,17 @@ defmodule MiniRiskManager.Ports.BalanceBlokerPort do
   @moduledoc """
   Behaviour/Port for block any malicious transfer
   """
+  @callback call_transfers(any()) :: any()
+
+  @spec call_transfers(any()) :: any()
+
+  def call_transfers(model_response) do
+    adapter().call_transfers(model_response)
+  end
+
+  defp adapter() do
+    :mini_risk_manager
+    |> Application.fetch_env!(__MODULE__)
+    |> Keyword.fetch!(:adapter)
+  end
 end
