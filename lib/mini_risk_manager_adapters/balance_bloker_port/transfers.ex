ce3d23b229d1061ebf3971403da494397a17e73b
Vitor Machado <vitor.machado@stone.com.br>
2022-07-21 16:00:15 -0300

    feat: create transfers adapter

--- lib/mini_risk_manager_adapters/balance_bloker_port/transfers.ex
+++ lib/mini_risk_manager_adapters/balance_bloker_port/transfers.ex
@@ -2,4 +2,14 @@ defmodule MiniRiskManagerAdapters.BalanceBlokerPort.Transfers do
   @moduledoc """
   Implements the behaviour of `MiniRiskManager.Ports.BalanceBlokerPort` to the Transfers
   """
+
+  use MiniRiskManagerAdapters.Tesla, "http://transfers.transfers"
+
+  @behaviour MiniRiskManager.Ports.ModelPort
+
+  @impl true
+  def call_model(%ModelInput{} = payload) do
+    "/service/v1/accounts/#{payload.operation_type}/block_balance"
+    |> post(payload)
+  end
 end
