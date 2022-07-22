f9a9460de442ceaba93ba42f4b068d9afea8d7d4
Vitor Machado <vitor.machado@stone.com.br>
2022-07-22 11:36:19 -0300

    feat: create transfers adapter

--- lib/mini_risk_manager_adapters/balance_bloker_port/transfers.ex
+++ lib/mini_risk_manager_adapters/balance_bloker_port/transfers.ex
@@ -8,7 +8,21 @@ defmodule MiniRiskManagerAdapters.BalanceBlokerPort.Transfers do
   @behaviour MiniRiskManager.Ports.BalanceBlokerPort
 
   @impl true
- def call_transfers() do
-  # post for url /service/v1/accounts/:account_id/block_balance
- end
+  def block_balance(payload) do
+    "/service/v1/accounts/#{payload.account_id}/block_balance"
+    |> post(payload)
+    |> handle_post()
+  end
+
+  defp handle_post({:ok, %Tesla.Env{status: 204}}), do: {:ok, :status}
+
+  defp handle_post({:ok, %Tesla.Env{status: status}}) do
+    Logger.error("#{__MODULE__}.handle_post status=#{inspect(status)}")
+    {:error, :request_failed}
+  end
+
+  defp handle_post({:error, reason}) do
+    Logger.error("#{__MODULE__}.handle_post error=#{inspect(reason)}")
+    {:error, :request_failed}
+  end
 end
