80d90c7fde223f876a410419c9df2e3ae2e1872a
Vitor Machado <vitor.machado@stone.com.br>
2022-07-25 15:23:50 -0300

    feat: create audit aggregate empty

---
+++ lib/mini_risk_manager/cashout/aggregates/audit_aggregate.ex
@@ -0,0 +1,15 @@
+defmodule MiniRiskManager.Cashout.Aggregates.AuditAggregate do
+  @moduledoc """
+  This Aggregates module takes care of Audit lifecycles
+  """
+
+  alias MiniRiskManager.Cashout.Models.Audit
+  alias MiniRiskManager.Repo
+
+  require Logger
+
+  # @spec create_audit(audit_params) ::
+  def create_audit(audit_params) do
+
+  end
+end
