e084d4c34aaca78749a435f7800b044f654a29a9
Vitor Machado <vitor.machado@stone.com.br>
2022-07-25 16:21:25 -0300

    refactor: include values in audits factory

--- test/support/factory/cashout/audits_factory.ex
+++ test/support/factory/cashout/audits_factory.ex
@@ -10,10 +10,32 @@ defmodule MiniRiskManager.Factory.Cashout.AuditsFactory do
       def mini_risk_manager_audit_factory do
         %Audit{
           id: Ecto.UUID.generate(),
-          input_params: %{},
-          model_input: %{},
-          model_response: %{},
-          is_valid: "true"
+          input_params: %{
+            operation_id: Ecto.UUID.generate,
+            operation_type: :inbound_pix_payment,
+            amount: 20,
+            account: {[
+              id: Ecto.UUID.generate,
+              balance: 20
+            ]},
+            target: {[
+              document: "Teste Factory",
+              account_code: "XPTO-0",
+              account_type: :CC
+            ]}
+          },
+          model_input: %{
+            operation_type: :inbound_pix_payment,
+            amount: 20,
+            balance: 20,
+            account_type: :CC,
+            sum_amount_last_24h: 40
+          },
+          model_response: %{
+            is_valid: true,
+            metadata: %{test: "Teste Factory"}
+          },
+          is_valid: true
         }
       end
     end
