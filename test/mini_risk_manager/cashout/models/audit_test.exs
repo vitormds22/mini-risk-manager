669c5a30c20af50458dbd753a1ab3cd0b3409ea0
Vitor Machado <vitor.machado@stone.com.br>
2022-07-25 19:17:39 -0300

    test: refactor audit test insert operations data

--- test/mini_risk_manager/cashout/models/audit_test.exs
+++ test/mini_risk_manager/cashout/models/audit_test.exs
@@ -8,28 +8,32 @@ defmodule MiniRiskManager.Cashout.Models.AuditTest do
 
   describe "create_changeset/1" do
     test "When missing required attrs return a invalid changeset" do
-      audit =
-        params_for(
+      params =
+        string_params_for(
           :mini_risk_manager_audit,
           input_params: nil,
           model_input: nil,
-          model_response: nil
+          model_response: nil,
+          operation_type: nil,
+          operation_id: nil
         )
 
       assert %Ecto.Changeset{
                valid?: false,
                errors: [
+                 operation_id: {@err_cant_be_blank, [validation: :required]},
+                 operation_type: {@err_cant_be_blank, [validation: :required]},
                  input_params: {@err_cant_be_blank, [validation: :required]},
                  model_input: {@err_cant_be_blank, [validation: :required]},
                  model_response: {@err_cant_be_blank, [validation: :required]}
                ]
-             } = Audit.create_changeset(audit)
+             } = Audit.create_changeset(params)
     end
 
     test "When passed all required attrs return a valid changeset" do
-      audit = params_for(:mini_risk_manager_audit)
+      params = string_params_for(:mini_risk_manager_audit)
 
-      assert %Ecto.Changeset{valid?: true} = Audit.create_changeset(audit)
+      assert %Ecto.Changeset{valid?: true} = Audit.create_changeset(params)
     end
   end
 end
