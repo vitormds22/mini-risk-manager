d6195cda56b088d73ff53f585610b6ac01eae090
Vitor Machado <vitor.machado@stone.com.br>
2022-07-27 10:52:22 -0300

    test: create a new test for operations data

--- test/mini_risk_manager/cashout/models/audit_test.exs
+++ test/mini_risk_manager/cashout/models/audit_test.exs
@@ -6,18 +6,32 @@ defmodule MiniRiskManager.Cashout.Models.AuditTest do
 
   @err_cant_be_blank "can't be blank"
 
-  describe "create_changeset/1" do
-    test "When missing required attrs return a invalid changeset" do
-      params =
-        string_params_for(
-          :mini_risk_manager_audit,
-          input_params: nil,
-          model_input: nil,
-          model_response: nil,
-          operation_type: nil,
-          operation_id: nil
-        )
+  setup do
+    params = string_params_for(:mini_risk_manager_audit)
 
+    invalid_params =
+      string_params_for(
+        :mini_risk_manager_audit,
+        input_params: nil,
+        model_input: nil,
+        model_response: nil,
+        operation_type: nil,
+        operation_id: nil
+      )
+
+    invalid_params_operation =
+      string_params_for(
+        :mini_risk_manager_audit,
+        input_params: nil,
+        operation_type: nil,
+        operation_id: nil
+      )
+
+    %{invalid_params_operation: invalid_params_operation, invalid_params: invalid_params, params: params}
+  end
+
+  describe "create_changeset/1" do
+    test "When missing required attrs return a invalid changeset", %{invalid_params: invalid_params} do
       assert %Ecto.Changeset{
                valid?: false,
                errors: [
@@ -27,13 +41,22 @@ defmodule MiniRiskManager.Cashout.Models.AuditTest do
                  model_input: {@err_cant_be_blank, [validation: :required]},
                  model_response: {@err_cant_be_blank, [validation: :required]}
                ]
-             } = Audit.create_changeset(params)
+             } = Audit.create_changeset(invalid_params)
     end
 
-    test "When passed all required attrs return a valid changeset" do
-      params = string_params_for(:mini_risk_manager_audit)
-
+    test "When passed all required attrs return a valid changeset", %{params: params} do
       assert %Ecto.Changeset{valid?: true} = Audit.create_changeset(params)
+    end
+
+    test "When pass a changeset without input params and operations data return a invalid changeset", %{invalid_params_operation: invalid_params_operation} do
+      assert %Ecto.Changeset{
+        valid?: false,
+        errors: [
+          operation_id: {@err_cant_be_blank, [validation: :required]},
+          operation_type: {@err_cant_be_blank, [validation: :required]},
+          input_params: {@err_cant_be_blank, [validation: :required]}
+        ]
+      } = Audit.create_changeset(invalid_params_operation)
     end
   end
 end
