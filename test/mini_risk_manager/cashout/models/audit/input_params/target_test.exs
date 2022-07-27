987060e2e6368093c19a784dcc6bc33792437e12
Vitor Machado <vitor.machado@stone.com.br>
2022-07-27 10:53:05 -0300

    test: create test for target struct

---
+++ test/mini_risk_manager/cashout/models/audit/input_params/target_test.exs
@@ -0,0 +1,36 @@
+defmodule MiniRiskManager.Cashout.Models.Audit.TargetTest do
+  @moduledoc false
+
+  use MiniRiskManager.DataCase, async: true
+  alias MiniRiskManager.Cashout.Models.Audit.InputParams.Target
+
+  @err_cant_be_blank "can't be blank"
+  setup do
+    params = string_params_for(:mini_risk_manager_audit)
+
+    invalid_params =
+      string_params_for(
+        :mini_risk_manager_audit_target_params,
+        document: nil,
+        account_code: nil,
+        account_type: nil
+      )
+
+    %{invalid_params: invalid_params, params: params}
+  end
+
+  describe "create_changeset/2" do
+    test "When passed all required attrs return a valid changeset", %{params: params} do
+      assert %Ecto.Changeset{valid?: true} = Target.create_changeset(params["input_params"]["target"])
+    end
+
+    test "When missing required attrs return a invalid changeset", %{invalid_params: invalid_params} do
+      assert %Ecto.Changeset{
+        valid?: false,
+        errors: [
+          document: {@err_cant_be_blank, [validation: :required]},
+        ]
+      } = Target.create_changeset(invalid_params)
+    end
+  end
+end
