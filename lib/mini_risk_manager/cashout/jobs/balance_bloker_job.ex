98de9c37dfcfeb240713c55e0f5ab275c7362910
Vitor Machado <vitor.machado@stone.com.br>
2022-08-02 18:30:28 -0300

    refactor: update and convert tupe operation type

--- lib/mini_risk_manager/cashout/jobs/balance_bloker_job.ex
+++ lib/mini_risk_manager/cashout/jobs/balance_bloker_job.ex
@@ -7,19 +7,23 @@ defmodule MiniRiskManager.Cashout.Jobs.BalanceBlokerJob do
   alias MiniRiskManager.Ports.Types.BalanceBlokerInput
 
   require Logger
+
   @impl Oban.Worker
-  def perform(%Oban.Job{args: %{
-    "operation_id" => operation_id,
-    "operation_type" => operation_type,
-    "amount" => amount,
-    "account_id" => account_id
-  }}) do
+  def perform(%Oban.Job{
+        args: %{
+          "operation_id" => operation_id,
+          "operation_type" => operation_type,
+          "amount" => amount,
+          "account_id" => account_id
+        }
+      }) do
     balance_bloker_input = %BalanceBlokerInput{
       operation_id: operation_id,
-      operation_type: operation_type,
+      operation_type: String.to_existing_atom(operation_type),
       amount: amount,
       account_id: account_id
     }
+
     BalanceBloker.run(balance_bloker_input)
   end
 end
