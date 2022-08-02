5765dfae35c27f08e0b806a4877ab5b596626be1
Vitor Machado <vitor.machado@stone.com.br>
2022-08-02 10:59:32 -0300

    test: add test when date time is out of range 24h

--- test/mini_risk_manager/cashout/repositories/audit_repository_test.exs
+++ test/mini_risk_manager/cashout/repositories/audit_repository_test.exs
@@ -7,7 +7,7 @@ defmodule MiniRiskManager.Cashout.Repositories.AuditRepositoryTest do
   setup do
     params = string_params_for(:mini_risk_manager_audit)
 
-    audit = insert(:mini_risk_manager_audit_insert)
+    audit = insert(:mini_risk_manager_audit)
 
     %{audit: audit, params: params}
   end
@@ -35,11 +35,11 @@ defmodule MiniRiskManager.Cashout.Repositories.AuditRepositoryTest do
     test "return the sum of all amounts at last 24h when have the same and different account_id", %{
       audit: %{input_params: input_params}
     } do
-      insert(:mini_risk_manager_audit_insert, inserted_at: NaiveDateTime.utc_now())
-      insert(:mini_risk_manager_audit_insert)
-      insert(:mini_risk_manager_audit_insert, inserted_at: NaiveDateTime.utc_now())
-      insert(:mini_risk_manager_audit_insert)
-      insert(:mini_risk_manager_audit_insert, inserted_at: NaiveDateTime.utc_now())
+      insert(:mini_risk_manager_audit)
+      insert(:mini_risk_manager_audit)
+      insert(:mini_risk_manager_audit)
+      insert(:mini_risk_manager_audit)
+      insert(:mini_risk_manager_audit)
 
       first_amount = input_params.amount
 
@@ -47,8 +47,41 @@ defmodule MiniRiskManager.Cashout.Repositories.AuditRepositoryTest do
 
       input_params = Map.put(input_params, :amount, second_amount)
 
-      assert %Audit{input_params: ^input_params} =
-               insert(:mini_risk_manager_audit_insert, %{input_params: input_params})
+      %Audit{input_params: ^input_params} =
+        insert(:mini_risk_manager_audit, %{input_params: input_params})
+
+      assert first_amount + second_amount ==
+               AuditRepository.sum_amount_last_24h(input_params.account.id)
+    end
+
+    test "with valid account_id but date time out of the range 24h", %{
+      audit: %{input_params: input_params} = audit
+    } do
+      first_amount = input_params.amount
+      second_amount = Enum.random(1..100)
+
+      first_date_time = audit.inserted_at
+      second_date_time = NaiveDateTime.add(first_date_time, -100)
+
+      third_date_time = NaiveDateTime.add(first_date_time, -86_500)
+      fourth_date_time = NaiveDateTime.add(first_date_time, 1000)
+
+      input_params = Map.put(input_params, :amount, second_amount)
+
+      %Audit{input_params: ^input_params} =
+        insert(:mini_risk_manager_audit, %{input_params: input_params})
+
+      audit = Map.put(audit, :inserted_at, second_date_time)
+
+      %Audit{} = insert(:mini_risk_manager_audit, %{inserted_at: second_date_time})
+
+      audit = Map.put(audit, :inserted_at, third_date_time)
+      %Audit{} = insert(:mini_risk_manager_audit, %{inserted_at: third_date_time})
+
+      audit = Map.put(audit, :inserted_at, fourth_date_time)
+      %Audit{} = insert(:mini_risk_manager_audit, %{inserted_at: fourth_date_time})
+
+      refute audit.inserted_at == first_date_time
 
       assert first_amount + second_amount ==
                AuditRepository.sum_amount_last_24h(input_params.account.id)
